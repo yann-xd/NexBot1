b7d6248fbf9bf3f8dca14b19baed890174cc0f0f
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-01 13:04:37 +0700

    Update bot on.js

--- plugins/bot on.js
+++ plugins/bot on.js
@@ -1,9 +1,10 @@
 let handler = async (m) => {
     global.db.data.chats[m.chat].isBanned = false
-    m.reply(' *Uaaaaawh🥱, Maap kak Kanna tadi ketiduran😊* ')
-}
+    m.reply(' *Uaaaaawh🥱, Maap kak tadi L-Botz ketiduran😊* ')
+}
+
 handler.tags = ['main']
-handler.command = /^(kannaon)$/i
+handler.command = /^(l-boton)$/i
 
 handler.admin = true
 
