9cf9d694037fc803b337e310225a17cd86cebf7c
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-01 14:11:00 +0700

    Update _menfess-Ans.js

--- plugins/_menfess-Ans.js
+++ plugins/_menfess-Ans.js
@@ -14,7 +14,7 @@ export async function before(m) {
 	if (!mf) return !0
 	console.log(m)
 	if (m.text === 'Balas Pesan' && m.quoted.mtype == 'buttonsMessage') return m.reply("Silahkan kirim pesan balasan kamu.");
-	let txt = `Hai kak @${mf.dari.split('@')[0]}, kamu menerima balasan nih.\n\nPesan balasannya:\n${m.text}\n`.trim();
+	let txt = `Hai kak, kamu menerima balasan nih.\n\nPesan balasannya:\n${m.text}\n`.trim();
 	await this.reply(mf.dari, txt, null).then(() => {
 		m.reply('Berhasil mengirim balasan.')
 		this.delay(1000)
