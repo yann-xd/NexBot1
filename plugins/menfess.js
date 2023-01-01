96f261e4cd2113f71ac9014f0e32ec033a954b6c
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-01 21:36:34 +0700

    Update menfess.js

--- plugins/menfess.js
+++ plugins/menfess.js
@@ -22,7 +22,7 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     if (mf) return !0
     try {
     	let id = + new Date
-        let txt = `Hai @${data.jid.split('@')[0]}, kamu menerima pesan Menfess nih.\n\nDari: *${name}*\nPesan: \n${pesan}\n\nMau balas pesan ini kak? bisa kak. kakak tinggal ketik pesan kakak nanti saya sampaikan ke *${name}*.`.trim();
+        let txt = `Hai kak, kamu menerima pesan Menfess nih.\n\nDari: *${name}*\nPesan: ${pesan}\n\nMau balas pesan ini kak? bisa kak. kakak tinggal ketik pesan kakak nanti saya sampaikan ke pengirim menfes.`.trim();
         await conn.sendButton(data.jid, txt, wm, ['Balas Pesan', '.balasmenfess'], null)
         .then(() => {
             m.reply('Berhasil mengirim pesan menfess.')
