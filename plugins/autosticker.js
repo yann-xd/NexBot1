130d5dc3706d7d2f4a166852998e16832e2a91e1
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-01 08:29:16 +0700

    Update autosticker.js

--- plugins/autosticker.js
+++ plugins/autosticker.js
@@ -17,9 +17,9 @@ function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
 let stikerhuuu = [
- "https://telegra.ph/file/dfbf483c209a31f01b4e5.png"//hengker2
- "https://telegra.ph/file/3007fd47512ba71a50c88.png"//doge
- "https://telegra.ph/file/72ab8915fb4d8eb534d1e.png"//gw udah muak
- "https://telegra.ph/file/ee6eb746f48d09bd945a7.png"//turu
- "https://telegra.ph/file/ac5def3b47309a2669f0e.png"//nggih
+ "https://telegra.ph/file/dfbf483c209a31f01b4e5.png",//hengker2
+ "https://telegra.ph/file/3007fd47512ba71a50c88.png",//doge
+ "https://telegra.ph/file/72ab8915fb4d8eb534d1e.png",//gw udah muak
+ "https://telegra.ph/file/ee6eb746f48d09bd945a7.png",//turu
+ "https://telegra.ph/file/ac5def3b47309a2669f0e.png",//nggih
 ]
