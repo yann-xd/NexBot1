051ffb50628665ff16484540b91173baa541199e
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-01 12:37:31 +0700

    Update _autoreact.js

--- plugins/_autoreact.js
+++ plugins/_autoreact.js
@@ -2,12 +2,12 @@ let handler = async (m, { conn, text }) => {
 
     conn.sendMessage(m.chat, {
           react: {
-            text: `${pickRandom(['😨','😅','😂','😳','😎', '🥵', '😱', '🐦', '🙄', '🐤','🗿','🐦','🤨','🥴','😐','👆','😔', '👀','👎'])}`,
+            text: `${pickRandom(['😓','😅','😂','🥴','👎'])}`,
             key: m.key,
           }})
   
   }
-handler.customPrefix = /(bile?k|ban?h|cum?|knt?l|y?|mmk|p|b(a|i)?c?(o|i)?(t|d)?|wibu|p(a)?nt(e)?k|pepe?k|owner)/i
+handler.customPrefix = /(bile?k|ban?h|cum?|knt?l|y?|mmk|p|b(a|i)?c?(o|i)?(t|d)?|wibu|p(a)?nt(e)?k|pepe?k|owner|hikmal|imal|hiqmal|mia|miaa|meaa|riann|rian)/i
 handler.command = new RegExp
   
 handler.mods = false
