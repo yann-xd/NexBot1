565157fb99bc2158440dc77234068ce5849a3db1
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-02 12:25:01 +0700

    Update nsfw-all1.js

--- plugins/nsfw-all1.js
+++ plugins/nsfw-all1.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, command }) => {
 let nyenye = `https://zenzapis.xyz/api/morensfw/${command}?apikey=${zenzkey}`
-    conn.sendButton(m.chat,` \`\`\`➩ Random Nsfw Rule34 ${text ? text.capitalize() : false}\`\`\` `, wm.date, res,['ɴᴇxᴛ',], m) 
+    conn.sendButton(m.chat,` \`\`\`➩ Random Nsfw Rule34 () : false}\`\`\` `, wm.date, res,['ɴᴇxᴛ',], m) 
 }
 handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri']
 handler.tags = ['nsfw']
