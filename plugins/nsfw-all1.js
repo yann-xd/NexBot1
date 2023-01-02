0751542b3d4314370184b8eddd63a821651d73f9
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-02 22:08:15 +0700

    Update nsfw-all1.js

--- plugins/nsfw-all1.js
+++ plugins/nsfw-all1.js
@@ -1,10 +1,9 @@
 let handler = async (m, { conn, command }) => {
-let nyenye = { return `https://zenzapis.xyz/api/morensfw/${command}?apikey=${zenzkey}`
-    conn.sendButtonImg(m.chat, nyenye, 'Nih', botdate, 'Next', `.${command}`, m) 
+let nyenye = `https://zenzapis.xyz/api/morensfw/${command}?apikey=${zenzkey}`
+    conn.sendButtonImg(m.chat, nyenye, 'Nih', wm2, 'Next', `.${command}`, m) 
 }
 handler.help = ['ahegao', 'ass', 'bdsm', 'blowjob', 'cum', 'ero', 'femdom', 'foot', 'gangbang', 'glasses', 'hentai', 'jahy', 'maid', 'manga', 'neko', 'orgy', 'panties', 'pussy','sfwneko', 'tentacles', 'thighs', 'yuri']
 handler.tags = ['nsfw']
 handler.command = /^(ahegao|ass|bdsm|blowjob|cum|ero|femdom|foot|gangbang|glasses|hentai|jahy|maid|manga|neko|orgy|panties|pussy|sfwneko|tentacles|thighs|yuri)$/i
-handler.premium = true
 //buatan hyzer, jgn hapus atuh 😊
-export default handler
+module.exports = handler
