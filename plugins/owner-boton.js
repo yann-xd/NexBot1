44774f6a2bfb939b0a5c09b910432691eaa4dc11
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-03 20:17:23 +0700

    Update owner-boton.js

--- plugins/owner-boton.js
+++ plugins/owner-boton.js
@@ -1,6 +1,6 @@
 let handler = async (m) => {
     global.db.data.chats[m.chat].isBanned = false
-    m.reply(' *Sukses Menghidupkan NexBotz 🤖* ')
+    m.reply(' *Sukses Menghidupkan L-Botz* ')
 }
 
 
