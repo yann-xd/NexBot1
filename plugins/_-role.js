c6683ad4f6ebec725ff6da61184f26817c29a58c
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-03 17:06:29 +0700

    Update _-role.js

--- plugins/_-role.js
+++ plugins/_-role.js
@@ -1,6 +1,6 @@
-const roles = {
+const tiers = {
     /*
-    'Role Name': <Minimal Level To Obtain this Role>
+    'Tiers Name': <Minimal Level To Obtain this Role>
     */
     'NPC': 0,
     'Normal': 1,
@@ -49,8 +49,8 @@ const roles = {
 export function before(m) {
         let user = db.data.users[m.sender]
         let level = user.level
-        let role = (Object.entries(roles).sort((a, b) => b[1] - a[1]).find(([, minLevel]) => level >= minLevel) || Object.entries(roles)[0])[0]
-        user.role = role
+        let tier = (Object.entries(tiers).sort((a, b) => b[1] - a[1]).find(([, minLevel]) => level >= minLevel) || Object.entries(tiers)[0])[0]
+        user.tier = tier
         return !0
     
 }
