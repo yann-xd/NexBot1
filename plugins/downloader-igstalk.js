ee55fce35ec4a231d52f929a793db16a84626629
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-04 09:12:32 +0700

    Update downloader-igstalk.js

--- plugins/downloader-igstalk.js
+++ plugins/downloader-igstalk.js
@@ -1,25 +1,67 @@
-import { iGstalk } from '../lib/stalkig.js'
+import fetch from 'node-fetch'
+import axios from 'axios'
+import cheerio from 'cheerio'
 
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args || !args[0]) throw `Gunakan format ${usedPrefix}${command} [username]\nContoh: ${usedPrefix}${command} jokowi`
-  let res = await iGstalk(args[0])
-  let arf = res
-  let iggs = `┏┉⌣ ┈̥-̶̯͡..̷̴✽̶┄┈┈┈┈┈┈┈┈┈┈┉┓
-┆ *INSTAGRAM STALK*
-└┈┈┈┈┈┈┈┈┈┈┈⌣ ┈̥-̶̯͡..̷̴
+let handler= async (m, { conn, args, usedPrefix, command }) => {
+    if (!args[0]) throw `Example use ${usedPrefix}${command} username`
+    let res = await igstalk(args[0]) 
+      
+   let username = res.username
+   let fullname = res.fullname
+   let post = res.post
+   let followe = res.followers
+   let followi = res.following
+   let bio = res.bio
+   let pepe = res.profile
 
-𝐔𝐬𝐞𝐫 𝐍𝐚𝗺𝐞: ${arf.username}
-𝐍𝐢𝐜𝐤 𝐍𝐚𝗺𝐞: ${arf.fullName}
-𝐅𝗼𝐥𝐥𝗼𝐰𝐞𝐫: ${arf.followers}
-𝐅𝗼𝐥𝐥𝗼𝐰𝐢𝐧𝐠: ${arf.following}
-𝐏𝗼𝐬𝐭𝐢𝐧𝐠: ${arf.postsCount}
-𝐁𝐢𝗼: ${arf.bio}
-𝐋𝐢𝐧𝐤 𝐔𝐬𝐞𝐫: https://instagram.com/${arf.username}`.trim()
-  conn.sendFile(m.chat, arf.profilePicHD, 'error.jpg', iggs, m)
+  let data = `
+💌 ᴜsᴇʀɴᴀᴍᴇ » 「 ${username} 」
+📧 ғᴜʟʟɴᴀᴍᴇ » 「 ${fullname} 」
+🎏 ${followe}  ғᴏʟʟᴏᴡᴇʀs
+🎀 ${followi}  ғᴏʟʟᴏᴡɪɴɢ
+📝 ᴘᴏsᴛ ${post} 
+📑 Bɪᴏ: ${bio}
+`.trim()
+
+  let pp = await(await fetch(pepe)).buffer() 
+  
+  conn.sendHydrated(m.chat, '「  sᴛᴀʟᴋɪɴɢ 」' , data, pp, `https://instagram.com/${username.replace(/^@/, '')}`, 'Link Profile', '0', 'Nothing', [
+
+      [null],
+
+      [null],
+
+      [null, null]
+
+    ], null,  { asLocation: true })
 }
-handler.help = ['igstalk <username>']
-handler.tags = ['tools']
+
+handler.help = ['igstalk'].map(v => v + ' <username>')
+handler.tags = ['internet']
+
 handler.command = /^(igstalk)$/i
-handler.limit = true
 
 export default handler
+
+async function igstalk(Username) {
+  return new Promise((resolve, reject) => {
+    axios.get('https://dumpor.com/v/'+Username, {
+      headers: {
+        "cookie": "_inst_key=SFMyNTY.g3QAAAABbQAAAAtfY3NyZl90b2tlbm0AAAAYWGhnNS1uWVNLUU81V1lzQ01MTVY2R0h1.fI2xB2dYYxmWqn7kyCKIn1baWw3b-f7QvGDfDK2WXr8",
+        "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"
+      }
+    }).then(res => {
+      const $ = cheerio.load(res.data)
+      const result = {
+        profile: $('#user-page > div.user > div.row > div > div.user__img').attr('style').replace(/(background-image: url\(\'|\'\);)/gi, ''),
+        fullname: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > div > a > h1').text(),
+        username: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > div > h4').text(),
+        post: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(1)').text().replace(' Posts',''),
+        followers: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(2)').text().replace(' Followers',''),
+        following: $('#user-page > div.user > div > div.col-md-4.col-8.my-3 > ul > li:nth-child(3)').text().replace(' Following',''),
+        bio: $('#user-page > div.user > div > div.col-md-5.my-3 > div').text()
+      }
+      resolve(result)
+    })
+  })
+}
