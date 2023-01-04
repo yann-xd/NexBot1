a42029ad326e3cae084c87538bac5f36aeb2a1b3
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-04 16:45:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -382,7 +382,7 @@ global.hwaifu = ['https://i.pinimg.com/originals/ed/34/f8/ed34f88af161e6278993e1
 'https://i.pinimg.com/originals/80/4f/1a/804f1a05f9996c96a2d492b4854b7fd5.jpg']
 /*============== STICKER WM ==============*/
 global.stickpack = 'ᴄʀᴇᴀᴛᴇᴅ ʙʏ'
-global.stickauth = `ʙᴏᴛᴢ`
+global.stickauth = `ʟ-ʙᴏᴛᴢ`
 
 global.multiplier = 38 // The higher, The harder levelup
 
