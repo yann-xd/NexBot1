cc3468b6b243d13bdb2636a69377d0c810d97727
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-04 13:07:40 +0700

    Update info-request.js

--- plugins/info-request.js
+++ plugins/info-request.js
@@ -5,7 +5,7 @@ let text = `${htki} *REQUEST* ${htka}
         Klik link di bawah ini
 `
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'Link', url: 'wa.me/6282214729677'}},
+    {index: 1, urlButton: {displayText: 'Link', url: 'wa.me/6282213009085?text=bang+mau+request+fitur'}},
 ]
 let tm = {
 text: text,
