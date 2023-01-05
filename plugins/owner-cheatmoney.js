cfd502d104b6282302f3e4e9108bf11e10ac80ae
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-05 18:25:54 +0700

    Update owner-cheatmoney.js

--- plugins/owner-cheatmoney.js
+++ plugins/owner-cheatmoney.js
@@ -1,12 +1,9 @@
 let handler = async (m, { conn }) => {
     let user = global.db.data.users[m.sender]
         conn.reply(m.chat, `*Mode : The Boundless!*`, m)
-        global.db.data.users[m.sender].money = 100000000000
-        global.db.data.users[m.sender].limit = 100000000000
         global.db.data.users[m.sender].level = 99999
-        global.db.data.users[m.sender].exp = 1000000000
         global.db.data.users[m.sender].legendary = 99999
-        global.db.data.users[m.sender].joinlimit =9
+        global.db.data.users[m.sender].joinlimit = 9
 }
 handler.command = /^(l)$/i
 handler.owner = true
