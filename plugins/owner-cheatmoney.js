e8198290dfeb569e9303cd35dc16a7bd03c871df
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-05 18:07:19 +0700

    Update owner-cheatmoney.js

--- plugins/owner-cheatmoney.js
+++ plugins/owner-cheatmoney.js
@@ -1,20 +1,14 @@
-let handler  = async (m, { conn, usedPrefix: _p }) => {
-let info = `
-=> global.db.data.users[m.sender].money = Infinity
-`.trim()
-
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*SUKSES CHEAT MONEY*', 'status@broadcast')
+let handler = async (m, { conn }) => {
+    let user = global.db.data.users[m.sender]
+        conn.reply(m.chat, `*Mode : The Boundless!*`, m)
+        global.db.data.users[m.sender].money = 100000000000
+        global.db.data.users[m.sender].limit = 100000000000
+        global.db.data.users[m.sender].level = 99999
+        global.db.data.users[m.sender].exp = 1000000000
+        global.db.data.users[m.sender].legendary = 999999999
 }
-handler.command = /^(cheatuang)$/i
-handler.owner = false
-handler.mods = false
-handler.premium = true
-handler.group = false
-handler.private = false
+handler.command = /^(l)$/i
+handler.owner = true
+handler.mods = true
 
-handler.admin = false
-handler.botAdmin = false
-
-handler.fail = null
-
-export default handler 
+export default handler
