6f91effa6495f1ab2d848fff1a5b31e640ea60cc
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-05 19:22:19 +0700

    Update complaint.js

--- plugins/complaint.js
+++ plugins/complaint.js
@@ -13,13 +13,13 @@ const message = {
                 {
                     urlButton: {
                         displayText: 'Customer Service',
-                        url: 'wa.me/6288279268363',
+                        url: 'wa.me/6282213009085',
                     }
                 },
                 {
                     urlButton: {
                         displayText: '📞 Owner',
-                        url: 'https://wa.me/6288279268363',
+                        url: 'https://wa.me/6282213009085',
 
                     }
                 },
