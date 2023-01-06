5bb9b2b03dbad9355acfb8c6ef8c2ce5b778692c
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-06 23:58:12 +0700

    Update _menfess-Ans.js

--- plugins/_menfess-Ans.js
+++ plugins/_menfess-Ans.js
@@ -8,7 +8,6 @@ export async function before(m) {
 	console.log({ text: m.text, type: m.quoted?.mtype })
 	if ((m.text === 'BALAS PESAN' || m.text === '') && m.quoted.mtype == 'buttonsMessage') return m.reply("Silahkan kirim pesan balasan kamu.\nKetik pesan sesuatu lalu kirim, maka pesan otomatis masuk ke target balas pesan.");
 	else {
-		let imgr = fla.getRandom()
 		let txt = `Hᴀɪ ᴋᴀᴋ @${mf.dari.split('@')[0]}, ᴋᴀᴍᴜ ᴍᴇɴᴇʀɪᴍᴀʜ ʙᴀʟᴇsᴀɴ ɴɪʜ.Pesan yang kamu kirim sebelumnya:\n${mf.pesan}\n\nPesan balasannya:\n${m.text}\n`.trim();
 		let sblm = `Pᴇsᴀɴ Aɴᴅᴀ Sᴇʙᴇʟᴜᴍɴʏᴀ ➛ ${mf.pesan}\nPᴇsᴀɴ Bᴀʟᴀsᴀɴɴʏᴀ ➨ ${m.text}`
 		await this.reply(mf.dari, txt, null).then(() => {
