2202d62d0be92302258b01e250f738567c603dce
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-06 15:48:20 +0700

    Update preset11.js

--- plugins/preset11.js
+++ plugins/preset11.js
@@ -3,8 +3,6 @@ import fetch from 'node-fetch'
 
 let handler = async (m, { conn, usedPrefix, isAdmin, isOwner, text }) => {
 	if (m.isGroup) {
-				break
-			case 'on': {
 				global.db.data.chats[m.chat].isBanned = false
 				conn.reply(m.chat, 'Ｓｕｋｓｅｓ', m, { contextInfo: { externalAdReply: {title: global.wm, body: 'ʙᴏᴛ ᴛᴇʟᴀʜ ᴅɪ ɴʏᴀʟᴀᴋᴀɴ ᴜɴᴛᴜᴋ ɢʀᴏᴜᴘ ɪɴɪ', sourceUrl: sig, thumbnail: fs.readFileSync('./thumbnail.jpg')  }}})
 }
