0878e107dd6be70df95699b3fa03526d5ca8b1ae
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-07 16:59:06 +0700

    Update menfess.js

--- plugins/menfess.js
+++ plugins/menfess.js
@@ -1,8 +1,19 @@
-handler = async (m, { conn, text, usedPrefix, command }) => {
+**
+ * Jangan Di Hapus!!
+ * 
+ * Buatan @SaipulAnuar (ᴹᴿ᭄ King Of Bear ×፝֟͜×)
+ * Youtube: https://youtu.be/pwLZpdfO8AU
+ * 
+ * Ingin bikin fitur tapi tidak bisa coding?
+ * hubungi: https://wa.me/6288279268363
+ * 
+ */
+
+let handler = async (m, { conn, text, usedPrefix, command }) => {
     conn.menfess = conn.menfess ? conn.menfess : {}
-    if (!text) throw `*Cara penggunaan :*\n\n${usedPrefix + command} nomor|nama pengirim|pesan\n\n*Note:* nama pengirim boleh nama samaran atau anonymous.\n\n*Contoh:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonymous|Hai.`;
+    if (!text) throw `*Cara penggunaan :*\n\n${usedPrefix + command} nomor|nama pengirim|pesan\n\n*Note:* nama pengirim boleh nama samaran atau anonymous.\n\n*Contoh:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Wajan|Halo.`;
     let [jid, name, pesan] = text.split('|');
-    if ((!jid || !name || !pesan)) throw `*Cara penggunaan :*\n\n${usedPrefix + command} nomor|nama pengirim|pesan\n\n*Note:* nama pengirim boleh nama samaran atau anonymous.\n\n*Contoh:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Anonymous|Hai.`;
+    if ((!jid || !name || !pesan)) throw `*Cara penggunaan :*\n\n${usedPrefix + command} nomor|nama pengirim|pesan\n\n*Note:* nama pengirim boleh nama samaran atau anonymous.\n\n*Contoh:* ${usedPrefix + command} ${m.sender.split`@`[0]}|Wajan|Halo.`;
     jid = jid.replace(/[^0-9]/g, '') + '@s.whatsapp.net';
     let data = (await conn.onWhatsApp(jid))[0] || {};
     if (!data.exists) throw 'Nomer tidak terdaftar di whatsapp.';
@@ -11,9 +22,8 @@ handler = async (m, { conn, text, usedPrefix, command }) => {
     if (mf) return !0
     try {
     	let id = + new Date
-        let tek = `Hai @${data.jid.split('@')[0]}, kamu menerima pesan Menfess nih.\n\nDari: *${name}*\nPesan: \n${pesan}\n\nMau balas pesan ini kak? bisa kok kak. tinggal ketik pesan kakak lalu kirim, nanti saya sampaikan ke *${name}*.`.trim();
-        let imgr = flaaa.getRandom()
-        await conn.sendButton(data.jid, bottime, tek, `${imgr + 'Menfess'}`, [['BALAS PESAN', '.balasmenfess']], fakes)
+        let txt = `Hai @${data.jid.split('@')[0]}, kamu menerima pesan Menfess nih.\n\nDari: *${name}*\nPesan: \n${pesan}\n\nMau balas pesan ini kak? bisa kak. kakak tinggal ketik pesan kakak nanti saya sampaikan ke *${name}*.`.trim();
+        await conn.sendButton(data.jid, txt, wm, ['Balas Pesan', '.balasmenfess'], null)
         .then(() => {
             m.reply('Berhasil mengirim pesan menfess.')
             conn.menfess[id] = {
@@ -31,15 +41,9 @@ handler = async (m, { conn, text, usedPrefix, command }) => {
         m.reply('eror');
     }
 }
-handler.tags = ['main']
-handler.help = ['menfess', 'menfes'].map(v => v + ' <nomor|nama pengirim|pesan>')
-handler.command = /^(menfess|menfes)$/i
+handler.tags = ['Menfess']
+handler.help = ['menfess', 'mfs'].map(v => v + ' <nomor|nama pengirim|pesan>')
+handler.command = /^(mfs|menfess|menfes)$/i
 handler.private = true
 
 export default handler
-
-/* Made By FokusDotId (Fokus ID)
- * https://github.com/FokusDotId
- * Ingin bikin fitur tapi tidak bisa coding?
- * hubungi: https://wa.me/6281320170984
-*/
