0ac05268e8c4e449876e923709aec21062814215
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-07 14:50:17 +0700

    Update anime-ppcp2.js

--- plugins/anime-ppcp2.js
+++ plugins/anime-ppcp2.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
 let anu = await fetch('https://api.akuari.my.id/randomimage/ppcouple')
 let url = await anu.json()
 let { hasil } = url 
-let weem = 'PutraModz'
+let weem = 'L-Botz'
 m.reply(wait)
 await conn.sendButton(m.chat, 'Lanang', weem, hasil.cowok, [['⇄ Next ⇄', `${usedPrefix + command}`]], m)
 await conn.sendButton(m.chat, 'Woman', weem, hasil.cewek, [['⇄ Next ⇄', `${usedPrefix + command}`]], m)
