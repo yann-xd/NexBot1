31f9b917f1cc76f1c7e4f267281b5beb582503ae
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-07 18:41:18 +0700

    Update tool-yuki.js

--- plugins/tool-yuki.js
+++ plugins/tool-yuki.js
@@ -2,9 +2,9 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 `.trim()
 
-conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*✧───────···────────✧*\n*Hallo Juga Kak, Yuki Kangen Nih* \n *Gimana Kabar Nya? >~<*', 'status@broadcast')
+conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*✧───────···────────✧*\n*Hallo Juga Kak, L Kangen Nih* \n *Gimana Kabar Nya? >~<*', 'status@broadcast')
 }
-handler.customPrefix = /^(Hallo Yuki|Hlo Yuki|hi Yuki|Hai Yuki)$/i
+handler.customPrefix = /^(Hallo L|l|Halo L|Oy L|)$/i
 handler.command = new RegExp
 
 handler.owner = false
