09b6a0bf3a0ad4f50719edada5b0ca877b5a74d5
yann-xd <103435665+yann-xd@users.noreply.github.com>
2023-01-09 12:48:54 +0700

    Update rpg-freegift.js

--- plugins/rpg-freegift.js
+++ plugins/rpg-freegift.js
@@ -1,16 +1,16 @@
 let handler = async(m, { conn, args, usedPrefix }) => {
 
     if (args.length == 0) return conn.reply(m.chat, `Harap masukan Kode FreeGiftmu!`, m)
-    if (args[0] == 'fangzbot_091' || args[0] == 'followfangz_' || args[0] == 'BloowwXx' || args[0] == 'BbL016JJQBCSr54OwwW0' || args[0] == 'giftkey01389320007' || args[0] == 'kode013923') {
+    if (args[0] == 'latif31' || args[0] == 'LBotz' || args[0] == 'December' || args[0] == '4v11y4' || args[0] == 'XyzVayy' || args[0] == 'D131221') {
 
     if (new Date - global.db.data.users[m.sender].lastgift > 86400000) {
-       conn.reply(m.chat, '*🎉 SELAMAT!*\nKamu telah mendapatkan\n1000 XP ✨\n 1 Limit! 🎫\n1000 Money 💹\n1 Potion 🥤', m)
-    global.db.data.users[m.sender].exp += 1000
-    global.db.data.users[m.sender].limit += 1
-    global.db.data.users[m.sender].money +=1000
-    global.db.data.users[m.sender].potion += 1
+       conn.reply(m.chat, '*🎉 SELAMAT!*\nKamu telah mendapatkan\n10000 XP ✨\n 5 Limit! 🎫\n10000 Money 💹\n3 Potion 🥤', m)
+    global.db.data.users[m.sender].exp += 10000
+    global.db.data.users[m.sender].limit += 5
+    global.db.data.users[m.sender].money +=10000
+    global.db.data.users[m.sender].potion += 3
     global.db.data.users[m.sender].lastclaim = new Date * 1
-} else conn.reply(m.chat, '[❗] Kode Gift Gratis hanya dapat digunakan sehari sekali ! dan kode hanya bisa di pakai sekali !\n\nKetik *!buygift* untuk membeli kodegift premium', m)
+} else conn.reply(m.chat, '[❗] Kode Gift Gratis hanya dapat digunakan sehari sekali ! dan kode hanya bisa di pakai sekali !\n\nKetik *!buygift* untuk membeli kodegift', m)
    } else {
         conn.reply(m.chat, `*「 KODE FREE TIDAK VALID 」*\n\nSilahkan daftar terlebih dahulu untuk mendapatkan kodegift gratis!\n\nKetik !daftar namamu|umurmu`, m)
     }
